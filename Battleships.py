653ede9edba78d73c5c174dba1f4ae440fe4b48d
Zombely <51829060+Zombely@users.noreply.github.com>
2020-01-13 00:02:50 +0100

    first commit

---
+++ Battleships.py
@@ -0,0 +1,91 @@
+import pygame
+
+# Nie ma implementacji do drugiego ekrany, trzeba zrobić podzział na dwie tablice bo sie wywala inaczej
+
+
+
+
+# Zdefiniowane zmienne
+WINDOWHEIGHT = 555
+WINDOWWIDTH = 555
+ROWS = 10
+COLUMNS = 10
+WIDTH = 50
+HEIGHT = 50
+MARGIN = 5
+
+# Kolory zdefiniowane
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+pygame.init()
+# Tworzy okno statków i pola ostrzału i nazywe gry
+display_ships = pygame.display.set_mode((WINDOWHEIGHT, WINDOWWIDTH))
+display_fire = pygame.display.set_mode((WINDOWWIDTH * 2, WINDOWHEIGHT))
+pygame.display.set_caption("Battleships")
+
+# Tworzy tablice gridu
+grid = []
+for row in range(ROWS):
+
+    grid.append([])
+    for column in range(COLUMNS):
+        grid[row].append(0)
+
+# Używany żeby sprawdzać jak szybko ekran się updatuje
+clock = pygame.time.Clock()
+
+
+class Ship():
+    def __init__(self,x,y,length,direction,grid):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.direction = direction
+        s_grid = grid
+
+
+# Główna pętla
+while True:
+
+    # Sprawdza eventy , zamknięcie X gry oraz klikniecie przyciskiem
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+            pos = pygame.mouse.get_pos()
+            # Zamienia koordynaty x/y na koordynaty gridu
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # ustawia w tablicy dana lokacje na 1 i wyswietla w konsoli współrzędne
+            grid[row][column] = 1
+            print("Click ", pos, "Grid coordinates: ", row, column)
+
+    # Ustawia tło
+    display_ships.fill(BLACK)
+
+
+    # Rysuje siatke
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            pygame.draw.rect(display_ships,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+
+
+    # Limit FPS
+    clock.tick(60)
+    pygame.display.flip()
+
+pygame.quit()
